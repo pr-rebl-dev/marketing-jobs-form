91ead293e25ab27388348f948a89c8de224638ed
pr-rebl-dev <phillip.rubery@rebl.co.nz>
2026-01-13 11:39:59 +1300

    Update form-auto-date.js

--- form-auto-date.js
+++ form-auto-date.js
@@ -2,14 +2,12 @@ document.addEventListener("DOMContentLoaded", function () {
   console.log("GitHub custom script loaded!");
 
   const input = document.getElementById("text-0000001a");
-    if (input) {
-      input.value = "123 Example Street";
-  
-      // Trigger events so 123FormBuilder registers the value
-      ["input", "change", "blur"].forEach(evt => {
-        input.dispatchEvent(new Event(evt, { bubbles: true }));
-      });
-    }
-  });
+  if (input) {
+    input.value = "123 Example Street";
 
+    // Trigger events so 123FormBuilder registers the value
+    ["input", "change", "blur"].forEach(evt => {
+      input.dispatchEvent(new Event(evt, { bubbles: true }));
+    });
+  }
 });
