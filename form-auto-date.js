d5138ea861e4385b20d618ca3b957ce71238d567
pr-rebl-dev <phillip.rubery@rebl.co.nz>
2026-01-13 12:06:08 +1300

    Update form-auto-date.js

--- form-auto-date.js
+++ form-auto-date.js
@@ -2,12 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
   console.log("GitHub custom script loaded!");
 
   setTimeout(() => {
-  const input = document.getElementById("text-0000001a");
-  if (input) {
-    input.value = "123 Example Street";
-    ["input","change","blur"].forEach(evt => input.dispatchEvent(new Event(evt,{bubbles:true})));
-  }else{
-    console.log("Field not found");
-}, 200); // Wait 200ms
-
+    const input = document.getElementById("text-0000001a");
+    if (input) {
+      input.value = "123 Example Street";
+      ["input", "change", "blur"].forEach(evt => 
+        input.dispatchEvent(new Event(evt, { bubbles: true }))
+      );
+      console.log('Property Address set to "123 Example Street"');
+    } else {
+      console.log("Field not found");
+    }
+  }, 200); // Wait 200ms
 });
